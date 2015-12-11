75ff80e15baa74cfe548c9e68f94b658732635b2
davidpazo <davidpazolopez@gmail.com>
2015-12-11 13:21:23 +0100

    ItemDetailFragment listo, onclick definido, interfaz y onattach

--- app/src/main/java/com/example/dpazolopez/examenandroid/ItemDetailFragment.java
+++ app/src/main/java/com/example/dpazolopez/examenandroid/ItemDetailFragment.java
@@ -1,12 +1,14 @@
 package com.example.dpazolopez.examenandroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.dpazolopez.examenandroid.dummy.DummyContent;
@@ -18,21 +20,12 @@ import com.example.dpazolopez.examenandroid.dummy.DummyContent;
  * on handsets.
  */
 public class ItemDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
+
     private DummyContent.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+
     public ItemDetailFragment() {
     }
 
@@ -41,9 +34,7 @@ public class ItemDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
+
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -59,11 +50,37 @@ public class ItemDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
+        //8 RECOGEMOS TEXTVIEW Y LO PONEMOS A CEERO
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+            ((TextView) rootView.findViewById(R.id.textView)).setText("Item " + mItem.id);
         }
 
+
+        Button button = (Button) rootView.findViewById(R.id.button);
+        button.setOnClickListener(this);
+
         return rootView;
+    }
+
+    /** onClick del boton */
+    @Override
+    public void onClick(View v) {
+        Listener.cerrar();
+    }
+
+    private MyFragmentListener Listener;
+
+    /** Aqui definimos el fragment que se va a cerrar desde la interfaz */
+
+    public interface MyFragmentListener {
+        void cerrar();
+    }
+    /** Cuando este fragment se una a la activity onAttach se ejecuta
+     * y al context se le pasa la activity */
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Listener = (MyFragmentListener) context;
     }
 }
